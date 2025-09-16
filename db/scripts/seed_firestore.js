331df381ebca4b70462c9ba1c6d00b56ddd5e9f7
Ratna Sai Aakanksha Yandagandi <145020794+Aakanksha2005@users.noreply.github.com>
2025-09-16 22:32:49 +0000

    seed_firestore.json

--- db/scripts/seed_firestore.js
+++ db/scripts/seed_firestore.js
@@ -2,7 +2,7 @@ import { initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
 import fs from "fs";
 
-const app = initializeApp();
+initializeApp();
 const db = getFirestore();
 
 async function seedRoutes() {
